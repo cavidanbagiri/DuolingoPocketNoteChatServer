409d4739f57e80da6c0eb8133f7d68f2126e036f
= <=>
2026-01-20 13:42:18 +0300

    set frontend url in production

--- server.js
+++ server.js
@@ -32,10 +32,16 @@ const io = socketIo(server, {
 });
 
 // Middleware
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
+
 app.use(express.json());
 
 // Apply socket authentication middleware
